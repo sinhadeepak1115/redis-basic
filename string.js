96604ac05926ee8ce0acc2d254efae375cba721d
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-02-02 17:27:24 +0530

    leraing ttl.

--- string.js
+++ string.js
@@ -1,6 +1,8 @@
 const client = require('./client')
 
 async function init() {
+  await client.set("age", "21");
+  await client.expire("age", 10)
   const result = await client.get("name");
   console.log("Result->", result)
 }
